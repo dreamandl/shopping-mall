e9fd9a54215e0e88ea42ada3060f3194f9a453b3
ll <2149821560@qq.com>
2024-05-25 13:53:40 +0800

    路由设计

---
+++ src/views/Home/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>我是home页</div>
+</template>
+  
+<script setup lang='ts'>
+  
+</script>
+  
+<style>
+  
+</style>
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    我是首页
+    <!-- 二级路由出口 -->
+    <RouterView/>
+    </div>
+</template>
+
+<script setup >
+
+</script>
+
+<style></style>
