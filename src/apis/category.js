6d3fcffc31bea9bd9d7cdf2868f3f1a0d9886b21
ll <2149821560@qq.com>
2024-05-28 18:24:31 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,8 +1,23 @@
 import request from '@/utils/http'
 
-export function getCategoryApi (id) {
+export function getCategoryApi(id) {
   return request({
     url: '/category',
+    params: {
+      id
+    }
+  })
+}
+
+/**
+ * @description: 获取二级分类列表数据
+ * @param {*} id 分类id 
+ * @return {*}
+ */
+
+export const getCategoryFilterApi = (id) => {
+  return request({
+    url: '/category/sub/filter',
     params: {
       id
     }
