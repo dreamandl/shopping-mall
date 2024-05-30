1adaf86208c634fb761f0ba99b5c30079a35bfad
ll <2149821560@qq.com>
2024-05-30 12:49:27 +0800

    登录-pinia管理用户数据

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -2,10 +2,12 @@
     // 表单校验（账号名+密码）
 
     import {ref} from 'vue'
-    import {loginApi} from '@/apis/user'
     import 'element-plus/theme-chalk/el-message.css' 
     import { ElMessage } from 'element-plus'
     import {useRouter} from 'vue-router'
+    import {useUserStore} from '@/stores/user'
+    const userStore = useUserStore()
+
     // 1.准备表单对象
     const form = ref({
       account:'',
@@ -48,8 +50,7 @@
         //以valid作为判断条件 如果通过校验才执行登录逻辑
         if(valid) {
           // TODO LOGIN
-          const res = await loginApi({account,password})
-          console.log(res)
+          await userStore.getUserInfo(account, password)
           // 1. 提示用户
           ElMessage({type:'success',message:'登录成功'})
           // 2. 跳转首页
