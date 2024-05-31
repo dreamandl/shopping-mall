910757115ce6d3864f82581445457cfc87bab7ab
ll <2149821560@qq.com>
2024-05-31 20:15:53 +0800

    退出登录时清除购物车数据

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -56,6 +56,11 @@ export const useCartStore = defineStore('cart', () => {
     }
   }
 
+  //清除购物车
+  const clearCart = () => {
+    cartList.value = []
+  }
+
 
   //单选功能
   const singleCheck = (skuId, selected) => {
@@ -89,6 +94,7 @@ export const useCartStore = defineStore('cart', () => {
     isAll,
     selectedCount,
     selectedPrice,
+    clearCart,
     addCart,
     delCart,
     singleCheck,
